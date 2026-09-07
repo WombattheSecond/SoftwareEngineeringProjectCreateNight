f8bc6848842be091123c7ac58379b6fcacbd6909
Peter Thomas <thomasp@macstudent.com.au>
2026-09-07 10:03:06 +1000

    UI Reframed

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def index():
     return render_template("index.html", error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8000)
+    app.run(debug=True, host="127.0.0.1", port=5000)
